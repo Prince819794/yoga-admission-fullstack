24b9c835303f874558e823dd39e5d09cd16e6aac
Prince819794 <saikiapp9@gmail.com>
2023-12-18 15:02:44 +0530

    first commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,7 @@
-// src/components/AdmissionForm.js
-
 import React, { useState } from "react";
 import axios from "axios";
-import "./App.css";
 import { toast } from "react-toastify";
+import "./App.css"; // Make sure to use the correct CSS file
 
 const AdmissionForm = () => {
   const [formData, setFormData] = useState({
@@ -23,83 +21,99 @@ const AdmissionForm = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     setLoading(true);
+
     try {
       // Call the Express backend API to store data
       const response = await axios.post(
         `${process.env.REACT_APP_BACKEND_URL}/api/enroll`,
         formData
       );
+
       // Assuming the API returns a success message
-      console.log(response);
       if (response.status === 200) {
         toast.success(response.data.data);
         setLoading(false);
-        setFormData({
-          name: "",
-          age: "",
-          selectedBatch: "",
-        });
+        resetForm();
       } else {
         toast.error(response.data.error);
         setLoading(false);
       }
     } catch (error) {
       console.error("Error submitting form:", error);
-      toast.error(error.response.data.error);
+      toast.error(error.response?.data?.error || "Something went wrong");
       setLoading(false);
     }
   };
 
+  const resetForm = () => {
+    setFormData({
+      name: "",
+      age: "",
+      selectedBatch: "",
+    });
+  };
+
   return (
     <div className="app">
-      {loading ? (
+      {loading && (
         <div className="loader">
           <div className="spinner"></div>
         </div>
-      ) : null}
-      <h2>Yoga Classes Admission Form</h2>
+      )}
       <form onSubmit={handleSubmit}>
-        <label>
-          Name:
-          <input
-            type="text"
-            name="name"
-            value={formData.name}
-            onChange={handleChange}
-          />
-        </label>
-        <br />
-        <label>
-          Age:
-          <input
-            type="number"
-            name="age"
-            value={formData.age}
-            onChange={handleChange}
-          />
-        </label>
-        <br />
-        <label>
-          Select Batch:
-          <select
-            name="selectedBatch"
-            value={formData.selectedBatch}
-            onChange={handleChange}
-          >
-            <option value="batch" defaultValue>
-              batches
-            </option>
-            <option value="6-7AM">6-7AM</option>
-            <option value="7-8AM">7-8AM</option>
-            <option value="8-9AM">8-9AM</option>
-            <option value="5-6PM">5-6PM</option>
-          </select>
-        </label>
-        <br />
+        <h2>Yoga Classes Admission Form</h2>
+        <FormField
+          label="Name:"
+          type="text"
+          name="name"
+          value={formData.name}
+          onChange={handleChange}
+        />
+        <FormField
+          label="Age:"
+          type="number"
+          name="age"
+          value={formData.age}
+          onChange={handleChange}
+        />
+        <FormField
+          label="Select Batch:"
+          type="select"
+          name="selectedBatch"
+          value={formData.selectedBatch}
+          onChange={handleChange}
+          options={[
+            { value: "batch", label: "Select batch" },
+            { value: "6-7AM", label: "6-7AM" },
+            { value: "7-8AM", label: "7-8AM" },
+            { value: "8-9AM", label: "8-9AM" },
+            { value: "5-6PM", label: "5-6PM" },
+          ]}
+        />
         <button type="submit">Submit</button>
       </form>
     </div>
   );
 };
+
+// A reusable form field component
+const FormField = ({ label, type, name, value, onChange, options }) => (
+  <div className="form-group">
+    <label>
+      {label}
+      {type === "select" ? (
+        <select name={name} value={value} onChange={onChange}>
+          {options.map((option) => (
+            <option key={option.value} value={option.value}>
+              {option.label}
+            </option>
+          ))}
+        </select>
+      ) : (
+        <input type={type} name={name} value={value} onChange={onChange} />
+      )}
+    </label>
+  </div>
+);
 
 export default AdmissionForm;
--- server/index.js
+++ server/index.js
@@ -1,93 +1,87 @@
-// server.js
-
 const express = require("express");
 const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
 const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 3030;
 
-// Connect to MongoDB
+app.use(express.json());
+app.use(cors());
+
 mongoose.connect(process.env.MONGODB_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
-// Define Mongoose schema
-const userSchema = new mongoose.Schema({
-  name: String,
-  age: Number,
-  selectedBatch: String,
-  paymentStatus: Boolean,
-  enrolledMonth: Number,
-});
+const userSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    age: { type: Number, required: true, min: 18, max: 65 },
+    selectedBatch: {
+      type: String,
+      required: true,
+      enum: ["6-7AM", "7-8AM", "8-9AM", "5-6PM"],
+    },
+    EnrolledMonth: {
+      type: String,
+    },
+    paymentStatus: { type: Boolean, default: false },
+  },
+  { timestamps: true }
+);
 
 const User = mongoose.model("User", userSchema);
 
-// Middleware
-app.use(bodyParser.json());
-app.use(cors());
+const completePayment = async (userId) => true;
 
-// Mock function for payment processing
-const completePayment = async (userId) => {
-  // Simulate payment processing logic
-  // In a real scenario, you would integrate with a payment gateway here
-
-  // For simplicity, let's assume the payment is successful
-  return true;
-};
-
-// API route to handle user enrollment with payment
 app.post("/api/enroll", async (req, res) => {
   try {
-    // Validate user data
     const { name, age, selectedBatch } = req.body;
 
+    // Input validation
     if (!name || !age || !selectedBatch) {
-      return res.status(400).json({ error: "Please fill all fields" });
+      console.error("Enrollment Error: Incomplete data provided");
+      return res
+        .status(400)
+        .json({ error: "Please fill all fields, including batch selection" });
     }
 
-    // Check age eligibility
+    // Age validation
     if (age < 18 || age > 65) {
+      console.error("Enrollment Error: Invalid age provided");
       return res
         .status(400)
         .json({ error: "Age must be between 18 and 65 to enroll." });
     }
 
-    // Check if the selected batch is valid
-    const validBatches = ["6-7AM", "7-8AM", "8-9AM", "5-6PM"];
-    if (!validBatches.includes(selectedBatch)) {
-      return res.status(400).json({ error: "Invalid batch selection." });
-    }
+    // Create a new user
+    const newUser = await User.create({ name, age, selectedBatch });
 
-    // Store user data in MongoDB
-    const newUser = new User({ name, age, selectedBatch });
-    await newUser.save();
-
-    // Perform payment
+    // Simulate payment and update payment status
     const paymentStatus = await completePayment(newUser._id);
 
-    // Update user payment status and enrollment details in the database
     if (paymentStatus) {
+      // Update user details with payment status and enrollment month
       await User.findByIdAndUpdate(newUser._id, {
         paymentStatus: true,
-        enrolledMonth: new Date().getMonth() + 1, // Store the month of enrollment
+        enrolledMonth: new Date().getMonth() + 1,
       });
-      res
+
+      console.log("Enrollment success:", newUser);
+      return res
         .status(200)
         .json({ data: "Enrollment successful. Payment completed." });
-    } else {
-      res.status(500).json({ error: "Payment failed." });
     }
+
+    console.error("Payment Error: Payment processing failed");
+    return res.status(500).json({ error: "Payment failed." });
   } catch (error) {
-    console.error("Error:", error);
+    console.error("Enrollment Error:", error);
     res.status(500).json({ error: "Internal Server Error" });
   }
 });
 
-// Start the server
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
